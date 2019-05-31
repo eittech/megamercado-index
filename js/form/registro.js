68c4ec3aad4a24ade694c388090f48318c4efe13
root <root@jesusgerard2008@gmail.com>
2019-05-31 17:08:12 -0400

    1160-jl Incorporado validador de campos

--- js/form/registro.js
+++ js/form/registro.js
@@ -2,14 +2,25 @@ $(function () {
 
     // Documento caracteristica
     $("input.send-form-data").click(function () {
-        $.post('php/contactform.php', $('#contactform').serialize(), function (response) {
-        	if(response == "EXISTE"){
-        		$("span.sms-title").text("Se encuentra registrado la Empresa");
-        	}else if(response == "OK"){
-        		$("span.sms-title").text("Se ha registrado con exito");
-        		$("#nombre, #correo, #empresa, #mensaje").val("");
-        	}
-    	});
+
+    	var nombre = $("#nombre");
+    	var correo = $("#correo");
+    	var empresa = $("#empresa");
+    	var mensaje = $("#mensaje");
+
+    	if(nombre.val() == "" && correo.val() == "" && empresa.val() == "" && mensaje.val() == ""){
+    		$("span.sms-title").text("Campo obligatorio");
+    	}else{
+	        $.post('php/contactform.php', $('#contactform').serialize(), function (response) {
+	        	if(response == "EXISTE"){
+	        		$("span.sms-title").text("Se encuentra registrado la Empresa");
+	        	}else if(response == "OK"){
+	        		$("span.sms-title").text("Se ha registrado con exito");
+	        		$("#nombre, #correo, #empresa, #mensaje").val("");
+	        	}
+	    	});
+    	}
+
     });
     
 });
